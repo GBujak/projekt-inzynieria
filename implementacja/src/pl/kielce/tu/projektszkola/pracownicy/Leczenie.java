bb2f01c649f2653703b4cc88465c5bf1678dc5df
Arek <s089012@student.tu.kielce.pl>
2020-12-02 21:32:17 +0100

    Zaktualizowano leczenie.

--- implementacja/src/pl/kielce/tu/projektszkola/pracownicy/Leczenie.java
+++ implementacja/src/pl/kielce/tu/projektszkola/pracownicy/Leczenie.java
@@ -4,6 +4,11 @@ package pl.kielce.tu.projektszkola.pracownicy;
 public class Leczenie {
     private int nrLegitymacji;
     private String opis;
+    private Pielegniarka pielegniarka;
 
     public String pobierzOpis(int nrLegitymacji){ return opis; }
+
+    public Pielegniarka getPielegniarka() { return pielegniarka; }
+
+    public void setPielegniarka(Pielegniarka pielegniarka) { this.pielegniarka=pielegniarka; }
 }
