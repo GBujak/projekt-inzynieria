07d2811a55e22f76bd314d341a72f9bec13cadc4
Grzegorz Bujak <s088943@student.tu.kielce.pl>
2020-12-02 20:27:50 +0100

    Użytkownik i Pracownik

---
+++ implementacja/src/pl/kielce/tu/projektszkola/Pracownik.java
@@ -0,0 +1,13 @@
+package pl.kielce.tu.projektszkola;
+
+public class Pracownik extends Uzytkownik {
+    private int zarobki;
+
+    public int getZarobki() {
+        return zarobki;
+    }
+
+    public void setZarobki(int zarobki) {
+        this.zarobki = zarobki;
+    }
+}
---
+++ implementacja/src/pl/kielce/tu/projektszkola/Uzytkownik.java
@@ -0,0 +1,41 @@
+package pl.kielce.tu.projektszkola;
+// Grzegorz Bujak
+
+public class Uzytkownik {
+    private String id;
+    private String imie;
+    private String nazwisko;
+    private String adres;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getAdres() {
+        return adres;
+    }
+
+    public void setAdres(String adres) {
+        this.adres = adres;
+    }
+}
