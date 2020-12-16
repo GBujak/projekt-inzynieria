b013e4cdb64f99e5364135f79c55526cf5aac67f
Daniel Jonczyk <daniel.jonczyk.inf@gmail.com>
2020-12-17 00:55:29 +0100

    dodano setter ostatniosprzatana

--- implementacja/src/pl/kielce/tu/projektszkola/zajecia/SalaLekcyjna.java
+++ implementacja/src/pl/kielce/tu/projektszkola/zajecia/SalaLekcyjna.java
@@ -5,11 +5,16 @@ import java.util.Date;
 public class SalaLekcyjna {
     private final Integer iloscMiejsc;
     private final String nazwa;
+
     private Date ostatnioSprzatana;
 
     public SalaLekcyjna(Integer iloscMiejsc, String nazwa){
             this.iloscMiejsc = iloscMiejsc;
             this.nazwa = nazwa;
+    }
+
+    public void setOstatnioSprzatana(Date ostatnioSprzatana) {
+        this.ostatnioSprzatana = ostatnioSprzatana;
     }
 
     public Integer getIloscMiejsc() {
