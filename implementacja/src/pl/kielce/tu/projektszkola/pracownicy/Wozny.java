80b263af23997745fc4336a87cc387857f5ceba8
Daniel Jonczyk <daniel.jonczyk.inf@gmail.com>
2020-12-17 00:55:55 +0100

    dodano metode posprzataj

--- implementacja/src/pl/kielce/tu/projektszkola/pracownicy/Wozny.java
+++ implementacja/src/pl/kielce/tu/projektszkola/pracownicy/Wozny.java
@@ -1,7 +1,14 @@
 package pl.kielce.tu.projektszkola.pracownicy;
 
 import pl.kielce.tu.projektszkola.Pracownik;
+import pl.kielce.tu.projektszkola.zajecia.SalaLekcyjna;
+
+import java.util.Date;
 
 public class Wozny extends Pracownik {
 
+    public void posprzataj(SalaLekcyjna sala){
+        Date data = new Date();
+        sala.setOstatnioSprzatana(data);
+    }
 }
