70df2a23c3ad7d8bc83d4db97843ddad414bfd1b
Daniel Jonczyk <daniel.jonczyk.inf@gmail.com>
2020-12-17 00:29:12 +0100

    przeniesiono pole random do funkcji utworz

--- implementacja/src/pl/kielce/tu/projektszkola/zajecia/SpotkanieOnline.java
+++ implementacja/src/pl/kielce/tu/projektszkola/zajecia/SpotkanieOnline.java
@@ -1,20 +1,19 @@
 package pl.kielce.tu.projektszkola.zajecia;
 
+import pl.kielce.tu.projektszkola.Uzytkownik;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
-import pl.kielce.tu.projektszkola.Uzytkownik;
 
 public class SpotkanieOnline {
     private Integer id;
     private String nazwa;
     private Zajecie zajecie;
     private List<Uzytkownik> uczestnicy;
-    private final Random rd;
 
     public SpotkanieOnline(){
-        rd = new Random();
+
     }
 
     public void dolacz(Uzytkownik uzytkownik){
@@ -25,7 +24,7 @@ public class SpotkanieOnline {
 
     public SpotkanieOnline utworz(String nazwa, Zajecie zajecie){
         Random rd = new Random();
-    	id = rd.nextInt();
+        id = rd.nextInt();
         this.zajecie = zajecie;
         uczestnicy = new ArrayList<>(uczestnicy);
         setNazwa(nazwa);
