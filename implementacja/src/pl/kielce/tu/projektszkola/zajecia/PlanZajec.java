7710413583b84aa467244ee890f54b0a5b55cfdd
Daniel Jonczyk <daniel.jonczyk.inf@gmail.com>
2020-12-17 00:26:55 +0100

    dodano metode zapisz i hashmapy do zapisu przedmiotow na planie

--- implementacja/src/pl/kielce/tu/projektszkola/zajecia/PlanZajec.java
+++ implementacja/src/pl/kielce/tu/projektszkola/zajecia/PlanZajec.java
@@ -1,7 +1,46 @@
 package pl.kielce.tu.projektszkola.zajecia;
 
+import pl.kielce.tu.projektszkola.dydaktyka.Klasa;
+import pl.kielce.tu.projektszkola.dydaktyka.Przedmiot;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.List;
+
 public class PlanZajec {
 
-    public void zatwierdz(){}
+    private Klasa klasa; //ktorej klasy dotyczy ten plan
 
+    HashMap< List<DayOfWeek>, List<LocalTime> > dniGodziny = new HashMap<>();
+    HashMap< HashMap< List<DayOfWeek>, List<LocalTime>>, List<Przedmiot> > dniGodzinyPrzedmioty = new HashMap<>();
+
+    private boolean czyZrobiony;
+    private boolean czyZatwierdzony;
+
+    public void zapisz(Klasa klasa, List<DayOfWeek> dni, List<LocalTime> godziny, List<Przedmiot> przedmioty){
+        this.klasa = klasa;
+        dniGodziny.put(dni, godziny);
+        dniGodzinyPrzedmioty.put(dniGodziny, przedmioty);
+    }
+
+    public void setCzyZrobiony(boolean czyZrobiony) {
+        this.czyZrobiony = czyZrobiony;
+    }
+
+    public void setCzyZatwierdzony(boolean czyZatwierdzony) {
+        this.czyZatwierdzony = czyZatwierdzony;
+    }
+
+    public boolean isCzyZatwierdzony() {
+        return czyZatwierdzony;
+    }
+
+    public boolean isCzyZrobiony() {
+        return czyZrobiony;
+    }
+
+    public Klasa getKlasa() {
+        return klasa;
+    }
 }
