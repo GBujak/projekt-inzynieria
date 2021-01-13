5cf94190b855440cf16ce1dea05e76c2354af4f0
Arek <s089012@student.tu.kielce.pl>
2021-01-13 18:44:07 +0100

    Dodano settery i datę leczenia.

--- implementacja/src/main/java/pl/kielce/tu/projektszkola/pracownicy/Leczenie.java
+++ implementacja/src/main/java/pl/kielce/tu/projektszkola/pracownicy/Leczenie.java
@@ -1,14 +1,18 @@
 package pl.kielce.tu.projektszkola.pracownicy;
 // Arkadiusz Markowski
 
+import java.util.Date;
+
 public class Leczenie {
     private int nrLegitymacji;
+    private Date data;
     private String opis;
     private Pielegniarka pielegniarka;
 
-    public String pobierzOpis(int nrLegitymacji){ return opis; }
-
-    public Pielegniarka getPielegniarka() { return pielegniarka; }
-
+    public int setNrLegitymacji(int nrLegitymacji) {this.nrLegitymacji = nrLegitymacji};
+    public Date setData(String Data) {this.data = data};
+    public String setOpis(String Opis) {this.opis = Opis};
     public void setPielegniarka(Pielegniarka pielegniarka) { this.pielegniarka=pielegniarka; }
+    public String pobierzOpis(int nrLegitymacji){ return opis; }
+    public Pielegniarka getPielegniarka() { return pielegniarka; }
 }
