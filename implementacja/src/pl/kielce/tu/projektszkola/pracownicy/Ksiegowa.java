91c829199512a8c374abf1387bdd1dc87b2bf863
Arek <s089012@student.tu.kielce.pl>
2021-01-13 19:37:54 +0100

    Pola zmienione na publiczne.

--- implementacja/src/pl/kielce/tu/projektszkola/pracownicy/Ksiegowa.java
+++ implementacja/src/pl/kielce/tu/projektszkola/pracownicy/Ksiegowa.java
@@ -6,8 +6,19 @@ import java.util.List;
 
 public class Ksiegowa extends Pracownik {
     private List<Faktura> faktury;
+    private Faktura faktura;
 
     public List<Faktura> getFaktury() { return faktury; }
-
+    public Faktuera getFaktura(){ return faktura; }
     public void setFaktury(List<Faktura> faktury) { this.faktury=faktury; }
+    public void setFaktura(int numer, int NIP, String przedmiotTransakcji, Integer kwota, Date data, String Sprzedawca, String Nabywca, boolean CzyZatwierdzony){
+        int numer;
+        int NIP;
+        String przedmiotTransakcji;
+        Integer kwota;
+        Date data;
+        String Sprzedawca;
+        String Nabywca;
+        faktura.CzyZatwierdzony =  CzyZatwierdzony;
+    }
 }
