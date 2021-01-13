d7fad7004a0ef5d890d077a0e9b2487b033abe2d
Arek <s089012@student.tu.kielce.pl>
2021-01-13 18:58:36 +0100

    Dodane setery i getery oraz dodatkowe pola faktury.

--- implementacja/src/main/java/pl/kielce/tu/projektszkola/pracownicy/Faktura.java
+++ implementacja/src/main/java/pl/kielce/tu/projektszkola/pracownicy/Faktura.java
@@ -1,20 +1,30 @@
 package pl.kielce.tu.projektszkola.pracownicy;
+//Arkadiusz Markowski
+
+import java.util.Date;
 
 public class Faktura {
-    private String numer;
-    private String NIP;
+    private int numer;
+    private int NIP;
     private String przedmiotTransakcji;
     private Integer kwota;
+    private Date data;
+    private String Sprzedawca;
+    private String Nabywca;
 
-    public String getNumer(){ return numer; }
-
-    public String getNIP(){ return NIP; }
-
+    public int getNumer(){ return numer; }
+    public int getNIP(){ return NIP; }
     public String getPrzedmiotTransakcji(){ return przedmiotTransakcji; }
-
     public Integer getKwota(){ return kwota; }
+    public Date getData(){ return data; }
+    public String getSprzedawca(){ return Sprzedawca; }
+    public String getNabywca(){ return Nabywca; }
 
-    public void zatwierdz(){}
-
-    public void odrzuc(){}
+    public int setNumer(int Numer){this.numer = Numer; }
+    public int setNIP(int NIP){this.NIP = NIP; }
+    public String setPrzedmiotTransakcji(String przedmiotTransakcji){this.przedmiotTransakcji = przedmiotTransakcji; }
+    public int setKwota(int kwota){ this.kwota = kwota; }
+    public Date setData(Date data){ this.data = data; }
+    public String setSprzedawca(String Sprzedawca){ this.Sprzedawca = Sprzedawca; }
+    public String setNabywca(String Nabywca){ this.Nabywca = Nabywca; }
 }
