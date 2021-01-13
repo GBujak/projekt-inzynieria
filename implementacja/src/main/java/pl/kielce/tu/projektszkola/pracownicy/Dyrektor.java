8ec0b18f82fe211b675d70f3b4cc7b40874e6f3c
Arek <s089012@student.tu.kielce.pl>
2021-01-13 20:34:52 +0100

    Zakomentowano planistę.

--- implementacja/src/main/java/pl/kielce/tu/projektszkola/pracownicy/Dyrektor.java
+++ implementacja/src/main/java/pl/kielce/tu/projektszkola/pracownicy/Dyrektor.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class Dyrektor extends Pracownik {
     private Ksiegowa ksiegowa;
     private Faktura faktura;
-    private Planista planista;
+    //private Planista planista;
 
     public List<Faktura> getFaktury() { return ksiegowa.getFaktury(); }
     public void zatwierdzFakture() { ksiegowa.getFaktura().setCzyZatwierdzony(true); }
-    public void zatwierdzPlan(){
+    /*public void zatwierdzPlan(){
         planista.getPlan().setCzyZatwierdzony(true);
-    }
+    }*/
 }
